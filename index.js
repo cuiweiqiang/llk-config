aaa7a32ed534c185cd2e9877dc7e54c2f6d2b232
cuiweiqiang <cuiweiqiang.dlnu@gmail.com>
2018-06-21 17:30:14 +0800

    init project

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+* @Author: cuiweiqiang
+* @Date:   2018-06-21 17:25:33
+* @Last Modified by:   cuiweiqiang
+* @Last Modified time: 2018-06-21 17:25:47
+*/
+'use strict';
+
+console.error('should not require llk-config');
+process.exit(1);
